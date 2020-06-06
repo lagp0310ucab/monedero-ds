0926d7c23da9082ac0a0e9c3215706ed795eaf16
Luis Alberto <laguerrero.17@est.ucab.edu.ve>
2020-06-06 17:50:14 -0400

    Cambio de Tab de Ayuda a Ajustes.

--- src/app/ajustes/ajustes-routing.module.ts
+++ src/app/ajustes/ajustes-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AyudaPage } from './ayuda.page';
+import { AjustesPage } from './ajustes.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: AyudaPage
+    component: AjustesPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AyudaPageRoutingModule {}
+export class AjustesPageRoutingModule {}
